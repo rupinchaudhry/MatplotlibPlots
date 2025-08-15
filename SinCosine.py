17217efab261b63739770cb57ba3f383b9dfc4b8
rupinchaudhry <briggenrupin@gmail.com>
2025-08-15 14:03:20 +0530

    Modified implmenetation of sin/cosine fucntion to get smoother curves

--- SinCosine.py
+++ SinCosine.py
@@ -1,10 +1,10 @@
-#Imports
+# Using Subplots
 import numpy as np
 import matplotlib.pyplot as plt
 
-x =  np.arange(0, 50, 1)
-y1 = np.sin(x)
-y2 = np.cos(x)
+x =  np.arange(0.0, 1.0, 0.01)
+y1 = np.sin(8*np.pi*x)
+y2 = np.cos(8*np.pi*x)
 fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(15,7))
 ax[0].plot(x, y1); ax[0].set_title("Sine wave")
 ax[1].plot(x, y2); ax[1].set_title("Cosine wave")
